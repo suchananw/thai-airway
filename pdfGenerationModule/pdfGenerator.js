3e20ac7a12bb31b667d6a3cdc04c1aff93e0a971
suchananw <suchananw@users.github.com>
2018-11-30 16:50:17 +0700

    chage pdf to puppeteer

--- pdfGenerationModule/pdfGenerator.js
+++ pdfGenerationModule/pdfGenerator.js
@@ -1,4 +1,5 @@
 const htmlToPdf = require("html-pdf");
+const puppeteer = require("puppeteer");
 const fs = require("fs");
 
 const config = {
@@ -7,19 +8,32 @@ const config = {
   paginationOffset: 2
 };
 
+// PDF not support css
+// function generatePDF(html, res) {
+//   const filename = "report-" + new Date().getTime() + ".pdf";
+//   return htmlToPdf.create(html, config).toBuffer(function(err, buffer) {
+//     console.log("genpdf This is a buffer:", Buffer.isBuffer(buffer));
+//     console.log("genpdf " + buffer);
+//     res.writeHead(200, {
+//       "Content-Type": "application/pdf",
+//       "Content-Disposition": "attachment; filename=" + filename,
+//       "Content-Length": buffer.length
+//     });
+//     res.end(buffer);
+//     // return Buffer.from(buffer, "binary");
+//   });
+// }
+
 function generatePDF(html, res) {
-  const filename = "report-" + new Date().getTime() + ".pdf";
-  return htmlToPdf.create(html, config).toBuffer(function(err, buffer) {
-    console.log("genpdf This is a buffer:", Buffer.isBuffer(buffer));
-    console.log("genpdf " + buffer);
-    res.writeHead(200, {
-      "Content-Type": "application/pdf",
-      "Content-Disposition": "attachment; filename=" + filename,
-      "Content-Length": buffer.length
-    });
-    res.end(buffer);
-    // return Buffer.from(buffer, "binary");
-  });
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(html);
+    const buffer = await page.pdf({ format: "A4" });
+    res.type("application/pdf");
+    res.send(buffer);
+    browser.close();
+  })();
 }
 
 module.exports = generatePDF;
