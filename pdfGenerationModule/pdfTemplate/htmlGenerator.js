c9f4bb6b21658ca351245f7516b8bababdbac1a7
suchananw <suchananw@users.github.com>
2018-12-01 16:03:09 +0700

    remove unused line from ejs

--- pdfGenerationModule/pdfTemplate/htmlGenerator.js
+++ pdfGenerationModule/pdfTemplate/htmlGenerator.js
@@ -4,6 +4,7 @@ function htmlGenerator(templateFile, data) {
   return new Promise(function(resolve, reject) {
     ejs.renderFile(templateFile, data, function(err, html) {
       if (err) {
+        console.log("html render error");
         reject(err);
       } else {
         console.log("html render success");
