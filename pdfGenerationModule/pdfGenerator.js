2f615f10fb2b54b9fe5f546cad72d0cc3fa3c802
suchananw <suchananw@users.github.com>
2018-12-03 10:32:24 +0700

    remove res content length

--- pdfGenerationModule/pdfGenerator.js
+++ pdfGenerationModule/pdfGenerator.js
@@ -8,17 +8,30 @@ const config = {
   paginationOffset: 2
 };
 
-// PDF not support css
+// // PDF not support css /with buffer
+// function generatePDF(html, res) {
+//   const filename = "report-" + new Date().getTime() + ".pdf";
+//   return htmlToPdf.create(html, config).toBuffer(function(err, buffer) {
+//     res.writeHead(200, {
+//       "Content-Type": "application/pdf",
+//       "Content-Disposition": "attachment; filename=" + filename,
+//       "Content-Length": buffer.length
+//     });
+//     res.end(buffer);
+//     // return Buffer.from(buffer, "binary");
+//   });
+// }
+
+// // PDF not support css /with stream
 function generatePDF(html, res) {
   const filename = "report-" + new Date().getTime() + ".pdf";
-  return htmlToPdf.create(html, config).toBuffer(function (err, buffer) {
-    const file = fs.writeFileSync(filename, buffer)
+  return htmlToPdf.create(html, config).toStream(function (err, stream) {
     res.writeHead(200, {
       "Content-Type": "application/pdf",
       "Content-Disposition": "attachment; filename=" + filename,
-      "Content-Length": file.length
     });
-    res.download(file);
+    stream.pipe(res)
+    // return Buffer.from(buffer, "binary");
   });
 }
 
