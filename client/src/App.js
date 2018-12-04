3b09dae97a13a58e59fe920046f65fed9241fc2d
suchananw <suchananw@users.github.com>
2018-12-04 16:15:34 +0700

    remove iframe

--- client/src/App.js
+++ client/src/App.js
@@ -70,9 +70,7 @@ class App extends Component {
           //Build a URL from the file
           const fileURL = URL.createObjectURL(file);
           //Open the URL on new Window
-          // window.open(fileURL);
-          var win = window.open();
-          win.document.write('<iframe src="' + fileURL + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>')
+          window.open(fileURL);
         })
           .catch(error => {
             console.log(error);
